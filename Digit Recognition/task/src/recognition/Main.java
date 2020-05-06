635960743afde59b2e2daab5761e85de739c5c58
Cristina Trifan <cristinamariatrifan@gmail.com>
2020-05-06 22:19:53 +0300

    commit for third stage of project

--- Digit Recognition/task/src/recognition/Main.java
+++ Digit Recognition/task/src/recognition/Main.java
@@ -1,18 +1,52 @@
 package recognition;
+import java.io.CharArrayWriter;
+import java.io.FileWriter;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
        Main obj = new Main();
+       int i = 0;
+
+        System.out.println("1. Learn the network");
+        System.out.println("2. Guess a number");
+        System.out.println("Your choice: ");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            if (scanner.hasNextInt()) {
+                i = scanner.nextInt();
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Please enter 1 or 2");
+        }
+
+        if (i == 1) {
+            //create random initial weights
+            //calculate output neurons
+            //calculate delta weight for each possible grid - use file with weights
+            //save weights to file
+
+            createCellsGrid();
+            obj.createInitialRandomWeights();
+            obj.writeDeltaWeights(obj.calculateDeltaWeights(obj.calculateOutputNeurons()));
+
+        }
+        else if (i == 2) {
 
        //create immutable lists with the weights values
-       Main.createWeights();
+       Main.createWeights();// will change with weights from file
 
         System.out.println("Input grid: ");
 
-       //calculate output based on weights and cells values //
-       obj.calcBestFit(obj.interpretCellvalues());
+       //calculate output based on weights and cells values
+       obj.calcBestFit(obj.interpretCellvalues());//will use sigmoid instead of bias
+        }
+
     }
+
+ //   public static List<Integer> getListWeights() { return listWeights; }
+ //   public static void setListWeights(List<Integer> listWeights) {Main.listWeights = listWeights;}
 
     private static List<Integer> listWeights0;
     private static List<Integer> listWeights1;
@@ -25,28 +59,128 @@ public class Main {
     private static List<Integer> listWeights8;
     private static List<Integer> listWeights9;
 
+    private static Map<Integer, List<Integer>> mapCellsGrid;
+
+    private List<Double> initialRandomWeights;
+
+    private void createInitialRandomWeights() {
+        Random random = new Random();
+        initialRandomWeights = new ArrayList<>();
+
+        for (int i = 0; i < 15; i++) {
+            initialRandomWeights.add(random.nextDouble());
+        }
+    }
+
+    //calculate the output neurons based on random initial weights:
+    private Map<Integer, Double> calculateOutputNeurons() {
+        Map<Integer, Double> outputNeurons = new HashMap<Integer, Double>();
+
+        //calculate based on formula On = S(isCelActiven * initialWeight)
+        for (int i = 0; i < 10; i++) {
+            Double outputNeuron = 0.0;
+            for (int j = 0; j < 15; j++) {
+                outputNeuron += mapCellsGrid.get(i).get(j) * initialRandomWeights.get(j);
+            }
+            //transform outputNeuron with sigmoid function
+            outputNeuron = 1 / (1 + Math.pow(Math.E, -outputNeuron));
+            //add neuron value to neuron list
+            outputNeurons.put(i, outputNeuron);
+        }
+
+        return outputNeurons;
+    }
+
+    //calculate delta weight for each possible grid - use file with weights
+    private Map<Integer, List<Double>> calculateDeltaWeights (Map<Integer, Double> outputNeurons) {
+        Map<Integer, List<Double>> mapDeltaWeights = new HashMap<>();
+
+        // for each output neuron
+        for (int i = 0; i < 10; i++) {
+            List<Double> listdeltaWeights = new ArrayList<>();
+            //for each ideal value of gridcells of the neuron
+                for (int j = 0; j < 15; j++) {
+                    Double deltaanon = 0.00;
+                    //for each activation value of the number we are testing
+                    for (int k = 0; k < 10; k++) {
+                        deltaanon =+ 0.5 * mapCellsGrid.get(k).get(j)
+                            * (mapCellsGrid.get(i).get(j) - outputNeurons.get(i));
+                }
+                    deltaanon = deltaanon / 10;
+                    listdeltaWeights.add(deltaanon);
+            }
+                mapDeltaWeights.put(i, listdeltaWeights);
+        }
+
+        return mapDeltaWeights;
+    }
+
+    private void writeDeltaWeights(Map<Integer, List<Double>> mapDeltaWeights) {
+        //C:\Users\3058\OneDrive - Q_PERIOR AG\Desktop\Inspiri\orga\deltaWeights.docx
+
+        try (FileWriter writer = new FileWriter("C:/Users/3058/OneDrive - Q_PERIOR AG/Desktop/Inspiri/orga/deltaWeights.txt", false)) {
+
+            for (var item : mapDeltaWeights.entrySet()) {
+                 writer.write(item.getKey().toString());
+                 writer.write(item.getValue().toString());
+            }
+
+          System.out.println(mapDeltaWeights.toString());
+        }
+        catch (Exception e) {
+            System.out.println("File writing error occurred");
+        }
+    }
+
     private static void createWeights() {
-        //15 weights and a bias - immutable lists
+        //15 weights and a bias
         listWeights0 = List.of(
-                1,1,1,1,-1,1,1,-1,1,1,-1,1,1,1,1,-1);
+                1,1,1,1,-1,1,1,-1,1,1,-1,1,1,1,1,-1,0);
         listWeights1 = List.of(
-                -1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,6);
+                -1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,6,1);
         listWeights2 = List.of(
-                1,1,1,-1,-1,1,1,1,1,1,-1,-1,1,1,1,1);
+                1,1,1,-1,-1,1,1,1,1,1,-1,-1,1,1,1,1,2);
         listWeights3 = List.of(
-                1,1,1,-1,-1,1,1,1,1,-1,-1,1,1,1,1,0);
+                1,1,1,-1,-1,1,1,1,1,-1,-1,1,1,1,1,0,3);
         listWeights4 = List.of(
-                1,-1,1,1,-1,1,1,1,1,-1,-1,1,-1,-1,1,2);
+                1,-1,1,1,-1,1,1,1,1,-1,-1,1,-1,-1,1,2,4);
         listWeights5 = List.of(
-                1,1,1,1,-1,-1,1,1,1,-1,-1,1,1,1,1,0);
+                1,1,1,1,-1,-1,1,1,1,-1,-1,1,1,1,1,0,5);
         listWeights6 = List.of(
-                1,1,1,1,-1,-1,1,1,1,1,-1,1,1,1,1,-1);
+                1,1,1,1,-1,-1,1,1,1,1,-1,1,1,1,1,-1,6);
         listWeights7 = List.of(
-                1,1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1,3);
+                1,1,1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,1,3,7);
         listWeights8 = List.of(
-                1,1,1,1,-1,1,1,1,1,1,-1,1,1,1,1,-2);
+                1,1,1,1,-1,1,1,1,1,1,-1,1,1,1,1,-2,8);
         listWeights9 = List.of(
-                1,1,1,1,-1,1,1,1,1,-1,-1,1,1,1,1,-1);
+                1,1,1,1,-1,1,1,1,1,-1,-1,1,1,1,1,-1,9);
+    }
+
+    private static void createCellsGrid() {
+        //10 lists x 15 activations of the ideal grid for each number 0-9
+
+        mapCellsGrid = Map.of(
+        0, List.of(
+                1,1,1,1,0,1,1,0,1,1,0,1,1,1,1),
+        1, List.of(
+                0,1,0,0,1,0,0,1,0,0,1,0,0,1,0),
+        2, List.of(
+                1,1,1,0,0,1,1,1,1,1,0,0,1,1,1),
+        3, List.of(
+                1,1,1,0,0,1,1,1,1,0,0,1,1,1,1),
+        4, List.of(
+                1,0,1,1,0,1,1,1,1,0,0,1,0,0,1),
+        5, List.of(
+                1,1,1,1,0,0,1,1,1,0,0,1,1,1,1),
+        6, List.of(
+                1,1,1,1,0,0,1,1,1,1,0,1,1,1,1),
+        7, List.of(
+                1,1,1,0,0,1,0,0,1,0,0,1,0,0,1),
+        8, List.of(
+                1,1,1,1,0,1,1,1,1,1,0,1,1,1,1),
+        9, List.of(
+                1,1,1,1,0,1,1,1,1,0,0,1,1,1,1)
+        );
     }
 
     private ArrayList<Integer> interpretCellvalues() {
